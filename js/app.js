99ea3c2e7fb68ec00142bee6b6fd788fd60b88a9
Lyubomir <lyubomir_vasilev1992@abv.bg>
2020-08-03 00:45:33 +0300

    Finished controllers for templates

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+import home from "../controllers/home.js";
+import register from "../controllers/register.js";
+import login from "../controllers/login.js";
+import catalog,  {create, edit, details} from "../controllers/movies.js";
+
+
+
+
 window.addEventListener('load', ()=> {
 
     const app = Sammy('#container', function(){
@@ -7,17 +15,21 @@ window.addEventListener('load', ()=> {
         this.get('/', home);
         this.get('index.html', home);
         this.get('#/home', home);
+
+
+        this.get("#/register", register);
+
+        this.get("#/login", login);
+
+        this.get("#/catalog", catalog);
+
+        this.get("#/create", create);
+
+        this.get("#/details/:id", details);
+
+        this.get("#/edit/:id", edit);
       
     });
 
     app.run();
-
-    async function home(){
-        this.partials = {
-            header: await this.load("./templates/common/header.hbs"),
-            footer: await this.load("./templates/common/footer.hbs")
-        }
-
-        this.partial("./templates/home.hbs",);
-    }
 });
